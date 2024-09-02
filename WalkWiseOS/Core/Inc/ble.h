b7b44519be68b07335906f800e01a423a9773faa
igorz <igor.zejcik@gmail.com>
2024-09-02 20:53:35 +0200

    Prototype function for sending data via UART

---
+++ WalkWiseOS/Core/Inc/ble.h
@@ -0,0 +1,14 @@
+/*
+ * ble.h
+ *
+ *  Created on: Aug 24, 2024
+ *      Author: igorz
+ */
+
+#ifndef SRC_BLE_H_
+#define SRC_BLE_H_
+
+int is_connected();
+int send_data();
+
+#endif /* SRC_BLE_H_ */
---
+++ WalkWiseOS/Debug/Core/Src/ble.d
@@ -0,0 +1,2 @@
+Core/Src/ble.o: ../Core/Src/ble.c ../Core/Inc/ble.h
+../Core/Inc/ble.h:
